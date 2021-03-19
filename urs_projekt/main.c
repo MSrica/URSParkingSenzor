58c1aeb7ec7bf2558dd8cd93b81a49b074bb7f2d
MSrica <srica.mateo@gmail.com>
2021-03-19 17:40:13 +0100

    kod da skonzultacija

--- urs_projekt/main.c
+++ urs_projekt/main.c
@@ -1,5 +1,6 @@
 #define F_CPU 7372800UL
-#define Trigger_pin	PA0							/* Trigger pin */
+#define Trigger_pin1 PA0							/* Trigger pin */
+#define Trigger_pin2 PA1							/* Trigger pin */
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -8,73 +9,87 @@
 #include <stdlib.h>
 #include "lcd.h"									/* Include LCD header file */
 
-int TimerOverflow = 0;
+uint8_t cnt = 0;
+uint32_t TimerOverflow = 0;
+char string[14];								/* Variables init */
+char string1[14];
+uint32_t distance = 0;
 
-ISR(TIMER1_OVF_vect){
-	TimerOverflow++;								/* Increment Timer Overflow count */
+ISR(TIMER0_COMP_vect){
+	TimerOverflow++;								/* Increment Timer Overflow count */	
 }
 
+ISR(INT0_vect){	
+	distance = (TimerOverflow * 3) / 2;
+	TimerOverflow = 0;
+	
+	lcd_clrscr();
+	if(cnt == 1){
+		//itoa(distance, string, 10);
+		dtostrf(distance, 4, 2, string);
+	}
+	if(cnt == 0){
+		//itoa(distance, string1, 10);
+		dtostrf(distance, 4, 2, string1);
+	}
+	lcd_gotoxy(0, 0);
+	lcd_puts(string);
+	lcd_gotoxy(0, 1);
+	lcd_puts(string1);
+	
+	TCNT0 = 0;
+	TCCR0 |= _BV(CS00);
+}
 
 int main(void){
 	DDRC = 0xff;									/* Buzzer port */
+	PORTC = 0xff;
 	
-	DDRD = _BV(4);									/* LCD port */
+	DDRD = _BV(4);					/* LCD port */
 	lcd_init(LCD_DISP_ON);							/* LCD init */
 	
-	char string[14];								/* Variables init */
-	long count;
-	double distance;
-	
-	DDRA = _BV(0); 									/* Make trigger pin as output */
+	//DDRA = _BV(0) | _BV(1); 									/* Make trigger pin as output */
 	PORTD = 0xFF;									/* Turn on Pull-up */
+					
+	TCCR0 = _BV(WGM01) | _BV(CS00);
+	OCR0 = 36;
 	
-	sei();											/* Enable global interrupt */
-	TIMSK = (1 << TOIE1);							/* Enable Timer1 overflow interrupts */
-	TCCR1A = 0;										/* Set all bit to zero Normal operation */
+	TCCR1A = _BV(COM1B1) | _BV(WGM10);
+	TCCR1B = _BV(WGM12) | _BV(CS11);
+	OCR1B = 64;
+													/* Enable global interrupt */
+	TIMSK = (1 << OCIE0);							/* Enable Timer1 overflow interrupts */
+	GICR = _BV(INT0);
+	MCUCR = _BV(ISC00);
+	sei();		
+
 
 	while(1){
-		TCCR1A = 0;									/* Set all bit to zero Normal operation */
-		count = 0;
-		distance = 0;
-		
-		PORTA |= (1 << Trigger_pin);				/* Give 10us trigger pulse on trig. pin to HC-SR04 */
-		_delay_us(10);
-		PORTA &= (~(1 << Trigger_pin));
-		
-		TCNT1 = 0;									/* Clear Timer counter */
-		TCCR1B = 0x41;								/* Capture on rising edge, No prescaler*/
-		TIFR = 1 << ICF1;							/* Clear ICP flag (Input Capture flag) */
-		TIFR = 1 << TOV1;							/* Clear Timer Overflow flag */
-
-		/* Calculate width of Echo by Input Capture (ICP) */
-		while ((TIFR & (1 << ICF1)) == 0);			/* Wait for rising edge */
-		TCNT1 = 0;									/* Clear Timer counter */
-		TCCR1B = 0x01;								/* Capture on falling edge, No prescaler */
-		TIFR = 1 << ICF1;							/* Clear ICP flag (Input Capture flag) */
-		TIFR = 1 << TOV1;							/* Clear Timer Overflow flag */
-		TimerOverflow = 0;							/* Clear Timer overflow count */
-
-		while ((TIFR & (1 << ICF1)) == 0);			/* Wait for falling edge */
-		count = ICR1 + (65535 * TimerOverflow);		/* Take count */
-		/* 8MHz Timer freq, sound speed =343 m/s */
-		distance = (double)count / 431.85;
-		
-		if(distance >= 16){
-			PORTC = 0x01;
-			}else{
-			PORTC = 0x00;
+//---------------------------------------------------------------------------------------------------------
+		if(cnt == 0){
+			//cnt = 1;
+			DDRA = _BV(0);
+			PORTA = 0x00;
+			PORTA |= (1 << Trigger_pin1);
+			_delay_us(10);
+			PORTA &= (~(1 << Trigger_pin1));
+		}else{
+			cnt = 0;
+			DDRA = _BV(1);
+			PORTA = 0x00;
+			PORTA |= (1 << Trigger_pin2);
+			_delay_us(10);
+			PORTA &= (~(1 << Trigger_pin2));
 		}
-		
-		
-		TCCR1A = _BV(COM1B1) | _BV(WGM10);
-		TCCR1B = _BV(WGM12) | _BV(CS11);
-		OCR1B = 64;
-		
-		
-		dtostrf(distance, 4, 2, string);			/* distance to string */
-		strcat(string, " cm   ");					/* Concat unit i.e.cm */
-		lcd_gotoxy(0, 0);
-		lcd_puts(string);
-		_delay_ms(250);
+		_delay_ms(65);
+
+		/*if(distance >= 16){
+			PORTC = 0x01;
+		}else{
+			PORTC = 0x00;
+		}*/
 	}
+	
+	
+	
 }
