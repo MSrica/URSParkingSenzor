5e6850f649276de4dbc28f3c8ef1f1b2f3b0e290
MSrica <srica.mateo@gmail.com>
2021-03-23 22:54:10 +0100

    added comments and todo, fixed variable names

--- urs_projekt/main.c
+++ urs_projekt/main.c
@@ -1,7 +1,25 @@
-#define F_CPU 7372800UL
-#define Trigger_pin	 PA0				
-#define Trigger_pin1 PA1			
+/* TODO
+IMPORTANT
+sometimes it doesn't start recording the distance - screen on but no readings, needed longer reset or disconnecting PD2 and connecting it after turning on board
+overflow - it just stops after some time
+displaying bars and not values
 
+KINDA IMPORTANT, HELPS
+figuring out every interrupt and explaining it in code/docs
+
+POLISHING
+keeping track of last known value(s) because of the big jumps(false readings)
+implementing LED lights somehow
+implementing button logic for increasing/decreasing buzzing distance
+*/
+
+
+// defining constants
+#define F_CPU 7372800UL
+#define Trigger_pin1 PA0				
+#define Trigger_pin2 PA1			
+
+// including libraries
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
@@ -9,70 +27,77 @@
 #include <stdlib.h>
 #include "lcd.h"						
 
-int TimerOverflow = 0;
+// declaring global variables
+uint32_t timerOverflow = 0;
+uint32_t count2 = 0;
+uint8_t interrupt0Turn = 0;
+uint8_t sensorTurn = 0;
+uint8_t buzzingDistance = 16;
 
+// timer 2 routine
 ISR(TIMER1_OVF_vect){
-	TimerOverflow++;					
+	timerOverflow++;					
 }
 
-static volatile int pulse = 0;
-static volatile int i = 0;
-
+// timer 0 routine
 ISR(INT0_vect){
-	if(i == 0){
-		TCCR1B |= 1<<CS10;
-		i = 1;
+	if(interrupt0Turn == 0){
+		TCCR1B |= 1 << CS10;
+		interrupt0Turn = 1;
 	}else{
 		TCCR1B = 0;
-		pulse = TCNT1;
+		count2 = TCNT1;
 		TCNT1 = 0;
-		i = 0;
+		interrupt0Turn = 0;
 	}
 }
 
-uint8_t cnt = 0;
-uint8_t initCnt = 0;
-
 int main(void){
+	// declaring variables
+	uint32_t count1 = 0;
+	uint16_t distance1 = 0;
+	uint16_t distance2 = 0;
+	char string1[16];
+	char string2[16];
+	
+	// buzzer initialization
 	DDRC = 0xff;
 	PORTC = 0xff;
 
+	// display initialization
 	DDRD = _BV(4);
+	PORTD = 0xFF;
 	lcd_init(LCD_DISP_ON);
 
-	char string[16];						
-	uint32_t count;
-	uint16_t distance;
-	uint32_t count_a = 0;
-	char show_a[16];
-
+	// trigger pins setup
 	DDRA = _BV(0) | _BV(1); 
-	PORTA = 0x00;								
-	PORTD = 0xFF;							
-								
+	PORTA = 0x00;															
+			
+	// interrupt registers initialization			
 	TIMSK = (1 << TOIE1);						
 	TCCR1A = 0;								
 	GICR |= 1 << INT0;
 	MCUCR |= 1 << ISC00;
 	sei();
 
+	// main loop
 	while(1){
-		if(cnt == 0){
-			if(initCnt > 10){
-				cnt = 1;
-			}
-			initCnt += 1;
+		if(sensorTurn == 0){
+			// setting up next sensor reading
+			sensorTurn = 1;
 			
+			// variables and registers reinitialization
 			TCCR1A = 0;
-			count = 0;
-			distance = 0;
+			count1 = 0;
+			distance1 = 0;
 
-			PORTA |= (1 << Trigger_pin);
+			// short pulse used for starting the ranging
+			PORTA |= (1 << Trigger_pin1);
 			_delay_us(10);
-			PORTA &= (~(1 << Trigger_pin));
+			PORTA &= (~(1 << Trigger_pin1));
 
-			TCNT1 = 0;
-			TCCR1B = 0x41;
+			TCNT1 = 0;									// NE ZNAM OBJASNIT ODOVUDA
+			TCCR1B = 0x41;								// UKLJUCUJUCI INTERRUPTE
 			TIFR = 1 << ICF1;
 			TIFR = 1 << TOV1;
 
@@ -81,37 +106,49 @@ int main(void){
 			TCCR1B = 0x01;
 			TIFR = 1 << ICF1;
 			TIFR = 1 << TOV1;
-			TimerOverflow = 0;
+			timerOverflow = 0;
 
 			while ((TIFR & (1 << ICF1)) == 0);
-			count = ICR1 + (65535 * TimerOverflow);
-			distance = (uint32_t)count / 431.85;
+			count1 = ICR1 + (65535 * timerOverflow);
+			distance1 = (uint32_t)count1 / 431.85;		// DO OVUDA
 			
+			// display register values
 			TCCR1A = _BV(COM1B1) | _BV(WGM10);
 			TCCR1B = _BV(WGM12) | _BV(CS11);
 			OCR1B = 64;
 
+			// printing values to display
 			lcd_clrscr();
-			itoa(distance,string,10);
-			itoa(count_a,show_a,10);
+			itoa(distance1,string1,10);
+			itoa(distance2,string2,10);
 			lcd_gotoxy(0, 0);
-			lcd_puts(string);
+			lcd_puts(string1);
 			lcd_gotoxy(0, 1);
-			lcd_puts(show_a);
-			_delay_ms(200);
-		}else if(initCnt > 10){
-			cnt = 0;
+			lcd_puts(string2);
+			_delay_ms(500);
+		}else{
+			// setting up next sensor reading
+			sensorTurn = 0;
+			
+			// registers reinitialization
 			TCCR1A = 0;
 			TCCR1B = 0;
 			TCNT1 = 0;
-			PORTA |= (1 << Trigger_pin1);
+			
+			// short pulse used for starting the ranging
+			PORTA |= (1 << Trigger_pin2);
 			_delay_us(10);
-			PORTA &= (~(1 << Trigger_pin1));
+			PORTA &= (~(1 << Trigger_pin2));
+			
+			// securing 60 ms pause in order to prevent noise
 			_delay_ms(60);
-			count_a = pulse/510;
+			
+			// calculating the distance
+			distance2 = count2 / 431.85;
 		}
 		
-		if(distance >= 16){
+		// buzzing the buzzer
+		if(distance1 >= buzzingDistance && distance2 >= buzzingDistance){
 			PORTC = 0x01;
 		}else{
 			PORTC = 0x00;
